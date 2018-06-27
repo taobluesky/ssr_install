683080b9e4801d992b7eaa6fc86bc9092c79eeff
hellotimo <taobluesky@qq.com>
2018-06-27 13:44:02 +0800

    fix bugs

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Install
 ```
 wget --no-check-certificate https://raw.githubusercontent.com/taobluesky/ssr_install/master/SSR.sh
 chmod +x SSR.sh
-./shadowsocksR.sh 2>&1
+./SSR.sh 2>&1
 ```
--- SSR.sh
+++ SSR.sh
@@ -16,7 +16,7 @@ echo
 
 libsodium_file="libsodium-1.0.16"
 libsodium_url="https://github.com/jedisct1/libsodium/releases/download/1.0.16/libsodium-1.0.16.tar.gz"
-shadowsocks_r_file="shadowsocksr-3.2.2"
+shadowsocks_r_file="ssr-3.2.2"
 shadowsocks_r_url="https://github.com/taobluesky/ssr/archive/3.2.2.tar.gz"
 
 #Current folder
